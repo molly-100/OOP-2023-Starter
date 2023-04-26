6b9cd1f1e891e2f1eeeee1b57afe5e4f98a03976
unknown <c21486544@mytudublin.ie>
2023-04-26 18:57:40 +0100

    update 4

--- java/src/ie/Word.java
+++ java/src/ie/Word.java
@@ -8,12 +8,12 @@ public class Word {
     public String word;
     public int count = 0;
 
-    ArrayList follow_ArrayList;
+    ArrayList<follow_ArrayList> follows;
 
     public void Word(String word, Follow follow_ArrayList)
     {
         this.word = word;
-        this.follow_ArrayList = follow_ArrayList;
+        this.follow_ArrayList = new follow_ArrayList<Follow>();
     }
 
     
